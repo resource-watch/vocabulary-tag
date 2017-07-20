9c02b6a706b5eba63c3fd28362d3dc22b5ac3dc7
rrequero <rrequero@gmail.com>
2017-07-20 13:31:06 +0200

    add include in favouriteS

--- app/src/routes/api/v1/favouriteRouter.js
+++ app/src/routes/api/v1/favouriteRouter.js
@@ -3,6 +3,7 @@ const logger = require('logger');
 const FavouriteSerializer = require('serializers/favouriteSerializer');
 const FavouriteModel = require('models/favourite');
 const FavouriteValidator = require('validators/favouriteValidator');
+const ctRegisterMicroservice = require('ct-register-microservice-node');
 
 const router = new Router({
     prefix: '/favourite'
@@ -22,6 +23,90 @@ class FavouriteRouter {
         }
 
         const data = yield FavouriteModel.find(filters);
+
+        if (this.query.include && this.query.include === 'true') {
+            logger.debug('including resources');
+            let widgets = [];
+            let datasets = [];
+            let layers = [];
+            data.map(resource => {
+                if (resource.resourceType === 'dataset') {
+                    datasets.push(resource.resourceId);
+                } else if (resource.resourceType === 'layer') {
+                    layers.push(resource.resourceId);
+                } else {
+                    widgets.push(resource.resourceId);
+                }
+            });
+            
+            try {
+                if (datasets.length > 0){
+                    logger.debug('Loading datasets');
+                    const datasetResources = yield ctRegisterMicroservice.requestToMicroservice({
+                        uri: `/dataset?ids=${datasets.join(',')}`,
+                        method: 'GET',
+                        json: true
+                    });
+                    for (let i = 0, length = datasetResources.data.length; i < length; i++) {
+                        const dataset = datasetResources.data[i];
+                        for (let j = 0, lengthData = data.length; j < lengthData; j++) {
+                            if (data[j].resourceType === 'dataset' && data[j].resourceId === dataset.id) {
+                                data[j] = data[j].toObject();
+                                data[j].resource = dataset;
+                                break;
+                            }
+                        }
+                    }                    
+                }
+                if (widgets.length > 0){
+                    logger.debug('Loading widgets');
+                    const widgetResources = yield ctRegisterMicroservice.requestToMicroservice({
+                        uri: `/widget/findByIds`,
+                        method: 'POST',
+                        json: true,
+                        body: {
+                            ids: widgets
+                        }
+                    });
+                    for (let i = 0, length = widgetResources.data.length; i < length; i++) {
+                        const widget = widgetResources.data[i];
+                        for (let j = 0, lengthData = data.length; j < lengthData; j++) {
+                            if (data[j].resourceType === 'widget' && data[j].resourceId === widget.id) {
+                                data[j] = data[j].toObject();
+                                data[j].resource = widget;
+                                break;
+                            }
+                        }
+                    }                    
+                }
+
+                if (layers.length > 0){
+                    logger.debug('Loading layers');
+                    const layerResources = yield ctRegisterMicroservice.requestToMicroservice({
+                        uri: `/layer/find-by-ids`,
+                        method: 'POST',
+                        json: true,
+                        body: {
+                            layer: {
+                                ids: layers
+                            }
+                        }
+                    });
+                    for (let i = 0, length = layerResources.data.length; i < length; i++) {
+                        const layer = layerResources.data[i];
+                        for (let j = 0, lengthData = data.length; j < lengthData; j++) {
+                            if (data[j].resourceType === 'layer' && data[j].resourceId === layer.id) {
+                                data[j] = data[j].toObject();
+                                data[j].resource = layer;
+                                break;
+                            }
+                        }
+                    }                    
+                }
+            } catch (err) {
+                logger.error(err);
+            }
+        }
 
         this.body = FavouriteSerializer.serialize(data);
 
--- app/src/serializers/favouriteSerializer.js
+++ app/src/serializers/favouriteSerializer.js
@@ -1,19 +1,35 @@
 const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 
-const favouriteSerializer = new JSONAPISerializer('favourite', {
-    attributes: ['userId', 'resourceType', 'resourceId', 'createdAt'],
-    typeForAttribute: function (attribute, record) {
-        return attribute;
-    },
-    keyForAttribute: 'camelCase'
-});
 
 class FavouriteSerializer {
 
-    static serialize(data) {
-        return favouriteSerializer.serialize(data);
+    static serializeElement(el) {
+        return {
+            id: el._id,
+            type: 'favourite',
+            attributes: {
+                userId: el.userId,
+                resourceType: el.resourceType,
+                resourceId: el.resourceId,
+                createdAt: el.createdAt,
+                resource: el.resource
+            }
+        };
     }
-    
+
+    static serialize(data) {
+        const result = {};
+        if (data) {
+            if (Array.isArray(data)) {
+                
+                result.data = data.map(el => FavouriteSerializer.serializeElement(el));
+            } else {
+                result.data = FavouriteSerializer.serializeElement(data);
+            }
+        }
+        return result;
+    }
+
 }
 
 module.exports = FavouriteSerializer;
