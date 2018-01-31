13b95bb37b491b28fff28fcbab5c364db75377fe
Sergio Gordillo <sgordillogallardo@gmail.com>
2018-01-31 13:17:12 +0100

    filter by fav

--- app/src/validators/favourite.validator.js
+++ app/src/validators/favourite.validator.js
@@ -8,6 +8,7 @@ class FavouriteValidator {
     static async validate(ctx) {
         logger.info('Validating Favourite Creation');
         ctx.checkBody('resourceType').notEmpty().in(RESOURCES);
+        ctx.checkBody('application').optional().toLow();
         ctx.checkBody('resourceId').notEmpty();
         if (ctx.errors) {
             logger.debug('errors ', ctx.errors);
@@ -15,10 +16,19 @@ class FavouriteValidator {
             ctx.status = 400;
             return;
         }
+        // App validation
+        if (ctx.request.body.application) {
+            if (ctx.request.body.loggedUser.extraUserData.apps.indexOf(ctx.request.body.application) <= -1) {
+                ctx.throw(403, 'Forbidden');
+            }
+        } else {
+            ctx.request.body.application = 'rw';
+        }
         const data = await FavouriteModel.findOne({
             resourceType: ctx.request.body.resourceType,
             resourceId: ctx.request.body.resourceId,
-            userId: ctx.request.body.loggedUser.id
+            userId: ctx.request.body.loggedUser.id,
+            application: ctx.request.body.application
         });
         if (data) {
             ctx.throw(400, 'Favourite duplicated');
