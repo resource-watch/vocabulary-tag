0e425512920282b7747bb3cdf1c96f8442ff408e
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-11-19 18:54:27 +0000

    Refactor mock generator methods for improved reusability

--- app/test/e2e/utils.js
+++ app/test/e2e/utils.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const nock = require('nock');
 
 const getUUID = () => Math.random().toString(36).substring(7);
 
@@ -56,202 +57,169 @@ const createResource = (app = 'rw', vocabularyCount = 1) => {
     };
 };
 
-/**
- * Mocks the requests for validating a dataset, returning the id of the mock dataset.
- *
- * @param {Object} opts Options object
- * Should contain the nock instance, and (optional) an id to use as the mock dataset's id.
- *
- * @returns {String} The id of the mock dataset.
- */
-const mockDataset = ({ nock, id = undefined }) => {
+const mockDataset = (id = undefined, extraData = {}) => {
     const idToUse = id || mongoose.Types.ObjectId();
-    nock(process.env.CT_URL)
-        .get(`/v1/dataset/${idToUse}`)
-        .reply(200, {
-            data: {
-                id: idToUse,
-                type: 'dataset',
-                attributes: {
-                    name: 'Uncontrolled Public-Use Airports -- U.S.',
-                    slug: 'Uncontrolled-Public-Use-Airports-US_2',
-                    type: null,
-                    subtitle: null,
-                    application: ['rw'],
-                    dataPath: null,
-                    attributesPath: null,
-                    connectorType: 'rest',
-                    provider: 'featureservice',
-                    userId: '1a10d7c6e0a37126611fd7a7',
-                    connectorUrl: 'https://services.arcgis.com/uDTUpUPbk8X8mXwl/arcgis/rest/services/Public_Schools_in_Onondaga_County/FeatureServer/0?f=json',
-                    tableName: 'Public_Schools_in_Onondaga_County',
-                    status: 'pending',
-                    published: true,
-                    overwrite: false,
-                    verified: false,
-                    blockchain: {},
-                    mainDateField: null,
-                    env: 'production',
-                    geoInfo: false,
-                    protected: false,
-                    legend: {
-                        date: [], region: [], country: [], nested: []
-                    },
-                    clonedHost: {},
-                    errorMessage: null,
-                    taskId: null,
-                    updatedAt: '2018-11-05T15:25:53.321Z',
-                    dataLastUpdated: null,
-                    widgetRelevantProps: [],
-                    layerRelevantProps: []
-                }
-            }
-        });
-    return idToUse;
+    const mockData = Object.assign({}, {
+        id: idToUse,
+        type: 'dataset',
+        attributes: {
+            name: 'Uncontrolled Public-Use Airports -- U.S.',
+            slug: 'Uncontrolled-Public-Use-Airports-US_2',
+            type: null,
+            subtitle: null,
+            application: ['rw'],
+            dataPath: null,
+            attributesPath: null,
+            connectorType: 'rest',
+            provider: 'featureservice',
+            userId: '1a10d7c6e0a37126611fd7a7',
+            connectorUrl: 'https://services.arcgis.com/uDTUpUPbk8X8mXwl/arcgis/rest/services/Public_Schools_in_Onondaga_County/FeatureServer/0?f=json',
+            tableName: 'Public_Schools_in_Onondaga_County',
+            status: 'pending',
+            published: true,
+            overwrite: false,
+            verified: false,
+            blockchain: {},
+            mainDateField: null,
+            env: 'production',
+            geoInfo: false,
+            protected: false,
+            legend: {
+                date: [], region: [], country: [], nested: []
+            },
+            clonedHost: {},
+            errorMessage: null,
+            taskId: null,
+            updatedAt: '2018-11-05T15:25:53.321Z',
+            dataLastUpdated: null,
+            widgetRelevantProps: [],
+            layerRelevantProps: []
+        }
+    }, extraData);
+    nock(process.env.CT_URL).get(`/v1/dataset/${idToUse}`).reply(200, { data: mockData });
+    return mockData;
 };
 
-/**
- * Mocks the requests for validating a widget, returning the id of the mock widget.
- *
- * @param {Object} opts Options object.
- * Should contain the nock instance, and (optional) an id to use as the mock widget's id.
- *
- * @returns {String} The id of the mock widget.
- */
-const mockWidget = ({ nock, id = undefined }) => {
+const mockWidget = (id = undefined, extraData = {}) => {
     const idToUse = id || mongoose.Types.ObjectId();
-    nock(process.env.CT_URL)
-        .get(`/v1/widget/${idToUse}`)
-        .reply(200, {
-            data: {
-                id: idToUse,
-                type: 'widget',
-                attributes: {
-                    name: 'Projected Change in Number of Frost-Free Season Days - Higher Emissions (A2)',
-                    dataset: '14813f7c-a635-4ed0-802f-3f4395daedd4',
-                    slug: 'projected-change-in-number-of-days-over-35-u-s-midwest',
-                    userId: '58333dcfd9f39b189ca44c75',
-                    description: 'Projected increase in annual average temperatures by mid-century (2041-2070)...',
-                    source: null,
-                    sourceUrl: null,
-                    authors: null,
-                    application: [
-                        'prep'
-                    ],
-                    verified: false,
-                    default: false,
-                    protected: false,
-                    defaultEditableWidget: false,
-                    published: true,
-                    freeze: false,
-                    env: 'production',
-                    queryUrl: 'ftp://filsrv.cicsnc.org/tsu/nca3-data/Figure_18-2_files.tar.gz',
-                    widgetConfig: {
-                        type: 'map',
-                        layer_id: '901ce25e-7707-4f98-98f5-a824386253ea'
-                    },
-                    template: false,
-                    layerId: null,
-                    createdAt: '2016-09-15T15:48:38.688Z',
-                    updatedAt: '2017-03-21T12:39:21.826Z'
-                },
-            }
-        });
-    return idToUse;
+    const mockData = Object.assign({}, {
+        id: idToUse,
+        type: 'widget',
+        attributes: {
+            name: 'Projected Change in Number of Frost-Free Season Days - Higher Emissions (A2)',
+            dataset: '14813f7c-a635-4ed0-802f-3f4395daedd4',
+            slug: 'projected-change-in-number-of-days-over-35-u-s-midwest',
+            userId: '58333dcfd9f39b189ca44c75',
+            description: 'Projected increase in annual average temperatures by mid-century (2041-2070)...',
+            source: null,
+            sourceUrl: null,
+            authors: null,
+            application: [
+                'prep'
+            ],
+            verified: false,
+            default: false,
+            protected: false,
+            defaultEditableWidget: false,
+            published: true,
+            freeze: false,
+            env: 'production',
+            queryUrl: 'ftp://filsrv.cicsnc.org/tsu/nca3-data/Figure_18-2_files.tar.gz',
+            widgetConfig: {
+                type: 'map',
+                layer_id: '901ce25e-7707-4f98-98f5-a824386253ea'
+            },
+            template: false,
+            layerId: null,
+            createdAt: '2016-09-15T15:48:38.688Z',
+            updatedAt: '2017-03-21T12:39:21.826Z'
+        },
+    }, extraData);
+    nock(process.env.CT_URL).get(`/v1/widget/${idToUse}`).reply(200, { data: mockData });
+    return mockData;
 };
 
-/**
- * Mocks the requests for validating a layer, returning the id of the mock layer.
- *
- * @param {Object} opts Options object.
- * Should contain the nock instance, and (optional) an id to use as the mock layer's id.
- *
- * @returns {String} The id of the mock layer.
- */
-const mockLayer = ({ nock, id = undefined }) => {
+const mockLayer = (id = undefined, extraData = {}) => {
     const idToUse = id || mongoose.Types.ObjectId();
-    nock(process.env.CT_URL)
-        .get(`/v1/layer/${idToUse}`)
-        .reply(200, {
-            data: {
-                id: idToUse,
-                type: 'layer',
-                attributes: {
-                    name: 'Heating Degree Days - Divisional',
-                    slug: 'heating-degree-days-divisional',
-                    dataset: '6e05863a-061a-418e-9f2f-3e0eaec3f0df',
-                    description: '',
-                    application: [
-                        'prep'
+    const mockData = Object.assign({}, {
+        id: idToUse,
+        type: 'layer',
+        attributes: {
+            name: 'Heating Degree Days - Divisional',
+            slug: 'heating-degree-days-divisional',
+            dataset: '6e05863a-061a-418e-9f2f-3e0eaec3f0df',
+            description: '',
+            application: [
+                'prep'
+            ],
+            iso: [
+                'USA'
+            ],
+            provider: 'arcgis',
+            userId: 'legacy',
+            default: true,
+            protected: false,
+            published: true,
+            env: 'production',
+            layerConfig: {
+                type: 'dynamicMapLayer',
+                body: {
+                    url: 'https://gis.ncdc.noaa.gov/arcgis/rest/services/cdo/indices/MapServer',
+                    layers: [
+                        10
                     ],
-                    iso: [
-                        'USA'
-                    ],
-                    provider: 'arcgis',
-                    userId: 'legacy',
-                    default: true,
-                    protected: false,
-                    published: true,
-                    env: 'production',
-                    layerConfig: {
-                        type: 'dynamicMapLayer',
-                        body: {
-                            url: 'https://gis.ncdc.noaa.gov/arcgis/rest/services/cdo/indices/MapServer',
-                            layers: [
-                                10
-                            ],
-                            useCors: false
-                        }
-                    },
-                    legendConfig: {
-                        type: 'choropleth',
-                        items: [
-                            {
-                                value: '< 300',
-                                color: '#B6EDF0'
-                            },
-                            {
-                                value: '300 - 600',
-                                color: '#8AC6EB'
-                            },
-                            {
-                                value: '600 - 900',
-                                color: '#5CA3E6'
-                            },
-                            {
-                                value: '900 - 1200',
-                                color: '#2183E0'
-                            },
-                            {
-                                value: '1200 - 1500',
-                                color: '#2158C7'
-                            },
-                            {
-                                value: '1500 - 1800',
-                                color: '#1B31AB'
-                            },
-                            {
-                                value: '> 1800',
-                                color: '#0B0BF5'
-                            }
-                        ]
-                    },
-                    interactionConfig: {},
-                    applicationConfig: {
-                        'config one': {
-                            type: 'lorem',
-                            from: {
-                                data: 'table'
-                            }
-                        }
-                    },
-                    staticImageConfig: {},
-                    createdAt: '2016-09-06T11:43:25.207Z',
-                    updatedAt: '2016-09-06T12:06:47.364Z'
+                    useCors: false
                 }
             },
-        });
-    return idToUse;
+            legendConfig: {
+                type: 'choropleth',
+                items: [
+                    {
+                        value: '< 300',
+                        color: '#B6EDF0'
+                    },
+                    {
+                        value: '300 - 600',
+                        color: '#8AC6EB'
+                    },
+                    {
+                        value: '600 - 900',
+                        color: '#5CA3E6'
+                    },
+                    {
+                        value: '900 - 1200',
+                        color: '#2183E0'
+                    },
+                    {
+                        value: '1200 - 1500',
+                        color: '#2158C7'
+                    },
+                    {
+                        value: '1500 - 1800',
+                        color: '#1B31AB'
+                    },
+                    {
+                        value: '> 1800',
+                        color: '#0B0BF5'
+                    }
+                ]
+            },
+            interactionConfig: {},
+            applicationConfig: {
+                'config one': {
+                    type: 'lorem',
+                    from: {
+                        data: 'table'
+                    }
+                }
+            },
+            staticImageConfig: {},
+            createdAt: '2016-09-06T11:43:25.207Z',
+            updatedAt: '2016-09-06T12:06:47.364Z'
+        }
+    }, extraData);
+    nock(process.env.CT_URL).get(`/v1/layer/${idToUse}`).reply(200, { data: mockData });
+    return mockData;
 };
 
 module.exports = {
