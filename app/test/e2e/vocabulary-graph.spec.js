e34c6970a9d2a88ef5ca923529ab7eaaf10085f0
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-12-02 16:08:02 +0000

    Add tests for vocabulary graph interactions where the Graph requests fail

--- app/test/e2e/vocabulary-graph.spec.js
+++ app/test/e2e/vocabulary-graph.spec.js
@@ -26,24 +26,32 @@ const assertCountOfVocabDatasetRelationships = async (datasetId, length) => {
         .send({ loggedUser: USERS.ADMIN }), length);
 };
 
-const putVocabDatasetRelationship = async (datasetId, vocabName, vocabData = {}) => {
+const postVocabDatasetRelationship = async (datasetId, vocabName, vocabData = {}) => {
+    const postData = {};
+    postData[vocabName] = vocabData;
+    mockPostGraphAssociation(datasetId);
+    return requester.put(`/api/v1/dataset/${datasetId}/vocabulary`)
+        .send({ ...postData, loggedUser: USERS.ADMIN });
+};
+
+const putVocabDatasetRelationship = async (datasetId, vocabName, vocabData = {}, graphSuccess = true) => {
     const putData = {};
     putData[vocabName] = vocabData;
-    mockPostGraphAssociation(datasetId);
+    mockPostGraphAssociation(datasetId, graphSuccess);
     return requester.put(`/api/v1/dataset/${datasetId}/vocabulary`)
         .send({ ...putData, loggedUser: USERS.ADMIN });
 };
 
-const patchVocabDatasetRelationship = async (datasetId, vocabName, vocabData = {}) => {
+const patchVocabDatasetRelationship = async (datasetId, vocabName, vocabData = {}, graphSuccess = true) => {
     mockDataset(datasetId);
-    mockPutGraphAssociation(datasetId);
+    mockPutGraphAssociation(datasetId, graphSuccess);
     return requester.patch(`/api/v1/dataset/${datasetId}/vocabulary/${vocabName}`)
         .send({ ...vocabData, loggedUser: USERS.ADMIN });
 };
 
-const deleteVocabDatasetRelationship = async (datasetId, vocabName) => {
+const deleteVocabDatasetRelationship = async (datasetId, vocabName, graphSuccess = true) => {
     mockDataset(datasetId);
-    mockDeleteGraphAssociation(datasetId);
+    mockDeleteGraphAssociation(datasetId, 'rw', graphSuccess);
     return requester
         .delete(`/api/v1/dataset/${datasetId}/vocabulary/${vocabName}?loggedUser=${JSON.stringify(USERS.ADMIN)}`)
         .send();
@@ -59,6 +67,25 @@ describe('Vocabulary interaction with Graph MS test suite', () => {
 
         await Resource.deleteMany().exec();
         await Vocabulary.deleteMany().exec();
+    });
+
+    it('POSTing vocab-dataset relationships with auth returns 200 OK with created data', async () => {
+        // Assert no relationships exist for the mock dataset created
+        const mockDatasetId = mockDataset().id;
+        await assertCountOfVocabDatasetRelationships(mockDatasetId, 0);
+
+        // POST one vocabulary relationship
+        const postResponse = await postVocabDatasetRelationship(
+            mockDatasetId,
+            'knowledge_graph',
+            { application: 'rw', tags: ['table'] },
+        );
+        assertOKResponse(postResponse);
+        postResponse.body.data[0].attributes.should.have.property('name').and.be.equal('knowledge_graph');
+        postResponse.body.data[0].attributes.should.have.property('tags').and.be.deep.equal(['table']);
+
+        // Assert that exists one relationship for the mock dataset created
+        await assertCountOfVocabDatasetRelationships(mockDatasetId, 1);
     });
 
     it('PUTting vocab-dataset relationships with auth returns 200 OK with created data', async () => {
@@ -147,6 +174,72 @@ describe('Vocabulary interaction with Graph MS test suite', () => {
         assertOKResponse(patchResponse);
         patchResponse.body.data[0].attributes.should.have.property('name').and.be.equal('knowledge_graph');
         patchResponse.body.data[0].attributes.should.have.property('tags').and.be.deep.equal(['vector']);
+    });
+
+    it('PUTting vocab-graph relationships when Graph request fails should return 200 OK with created data', async () => {
+        // Assert no relationships exist for the mock dataset created
+        const mockDatasetId = mockDataset().id;
+        await assertCountOfVocabDatasetRelationships(mockDatasetId, 0);
+
+        // PUT one vocabulary relationship
+        const putResponse = await putVocabDatasetRelationship(
+            mockDatasetId,
+            'knowledge_graph',
+            { application: 'rw', tags: ['table'] },
+            false,
+        );
+        assertOKResponse(putResponse);
+        putResponse.body.data[0].attributes.should.have.property('name').and.be.equal('knowledge_graph');
+        putResponse.body.data[0].attributes.should.have.property('tags').and.be.deep.equal(['table']);
+
+        // Assert that exists one relationship for the mock dataset created
+        await assertCountOfVocabDatasetRelationships(mockDatasetId, 1);
+    });
+
+    it('PATCHing vocab-graph relationships when Graph request fails should return 200 OK with updated data', async () => {
+        // PUT one vocabulary relationship
+        const mockDatasetId = mockDataset().id;
+        assertOKResponse(await putVocabDatasetRelationship(
+            mockDatasetId,
+            'knowledge_graph',
+            { application: 'rw', tags: ['table'] },
+        ));
+
+        // Assert that exists one relationship for the mock dataset created
+        await assertCountOfVocabDatasetRelationships(mockDatasetId, 1);
+
+        // PATCH the previously created relationship -- with error!
+        const patchResponse = await patchVocabDatasetRelationship(
+            mockDatasetId,
+            'knowledge_graph',
+            { application: 'rw', tags: ['vector'] },
+            false,
+        );
+        assertOKResponse(patchResponse);
+        patchResponse.body.data[0].attributes.should.have.property('name').and.be.equal('knowledge_graph');
+        patchResponse.body.data[0].attributes.should.have.property('tags').and.be.deep.equal(['vector']);
+
+        // Assert that still exists one relationship for the mock dataset updated
+        await assertCountOfVocabDatasetRelationships(mockDatasetId, 1);
+    });
+
+    it('DELETing vocab-graph relationships when Graph request fails should return 200 OK with no data', async () => {
+        // PUT one vocabulary relationship
+        const mockDatasetId = mockDataset().id;
+        assertOKResponse(await putVocabDatasetRelationship(
+            mockDatasetId,
+            'knowledge_graph',
+            { application: 'rw', tags: ['table'] },
+        ));
+
+        // Assert that exists one relationship for the mock dataset created
+        await assertCountOfVocabDatasetRelationships(mockDatasetId, 1);
+
+        // DELETE the previously created relationship
+        assertOKResponse(await deleteVocabDatasetRelationship(mockDatasetId, 'knowledge_graph', false));
+
+        // Assert that no relationship exists for the mock dataset created
+        await assertCountOfVocabDatasetRelationships(mockDatasetId, 0);
     });
 
     afterEach(async () => {
