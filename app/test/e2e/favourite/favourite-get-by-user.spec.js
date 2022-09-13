e790f3bbbc7fadeac865502a3fdbb1bc06cfbe45
Ruben Vallejo <ralexanderv89@gmail.com>
2022-09-13 17:53:46 +0200

    feat: changes favouritesByUser endpoint to accept all filtering

--- app/test/e2e/favourite/favourite-get-by-user.spec.js
+++ app/test/e2e/favourite/favourite-get-by-user.spec.js
@@ -82,6 +82,81 @@ describe('Find favourites by user', () => {
         response.body.data[2].attributes.resourceId.should.equal(favouriteThree.resourceId);
     });
 
+    it('Find favourite by userId being authenticated and userId in body but not app returns favourite data from rw app', async () => {
+        mockGetUserFromToken(USERS.USER);
+        const favouriteOne = await (new Favourite(createFavourite({ userId: USERS.USER.id }))).save();
+        const favouriteTwo = await (new Favourite(createFavourite({ userId: USERS.USER.id, resourceType: 'widget' }))).save();
+        await (new Favourite(createFavourite({ userId: USERS.USER.id, resourceType: 'layer', application: 'gfw' }))).save();
+
+        const response = await requester
+            .post(`/api/v1/favourite/find-by-user`)
+            .set('Authorization', `Bearer abcd`)
+            .send({ userId: USERS.USER.id });
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').with.lengthOf(2);
+
+        response.body.data[0].id.should.equal(favouriteOne._id.toString());
+        response.body.data[0].attributes.application.should.equal('rw');
+        response.body.data[0].attributes.resourceType.should.equal(favouriteOne.resourceType);
+        response.body.data[0].attributes.resourceId.should.equal(favouriteOne.resourceId);
+
+        response.body.data[1].id.should.equal(favouriteTwo._id.toString());
+        response.body.data[1].attributes.application.should.equal('rw');
+        response.body.data[1].attributes.resourceType.should.equal(favouriteTwo.resourceType);
+        response.body.data[1].attributes.resourceId.should.equal(favouriteTwo.resourceId);
+    });
+
+    it('Find favourite by userId being authenticated and userId in body with app returns favourite data from included app', async () => {
+        mockGetUserFromToken(USERS.USER);
+        await (new Favourite(createFavourite({ userId: USERS.USER.id }))).save();
+        await (new Favourite(createFavourite({ userId: USERS.USER.id, resourceType: 'widget' }))).save();
+        const gfwFavourite = await (new Favourite(createFavourite({ userId: USERS.USER.id, resourceType: 'layer', application: 'gfw' }))).save();
+
+        const response = await requester
+            .post(`/api/v1/favourite/find-by-user`)
+            .set('Authorization', `Bearer abcd`)
+            .send({ userId: USERS.USER.id, application: 'gfw' });
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').with.lengthOf(1);
+
+        response.body.data[0].id.should.equal(gfwFavourite._id.toString());
+        response.body.data[0].attributes.application.should.equal('gfw');
+        response.body.data[0].attributes.resourceType.should.equal(gfwFavourite.resourceType);
+        response.body.data[0].attributes.resourceId.should.equal(gfwFavourite.resourceId);
+    });
+
+    it('Find favourite by userId being authenticated and userId and application with all returns favourite data from every app', async () => {
+        mockGetUserFromToken(USERS.USER);
+        const favouriteOne = await (new Favourite(createFavourite({ userId: USERS.USER.id }))).save();
+        const favouriteTwo = await (new Favourite(createFavourite({ userId: USERS.USER.id, resourceType: 'widget' }))).save();
+        const favouriteThree = await (new Favourite(createFavourite({ userId: USERS.USER.id, resourceType: 'layer', application: 'gfw' }))).save();
+
+        const response = await requester
+            .post(`/api/v1/favourite/find-by-user`)
+            .set('Authorization', `Bearer abcd`)
+            .send({ userId: USERS.USER.id, application: 'all' });
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').with.lengthOf(3);
+
+        response.body.data[0].id.should.equal(favouriteOne._id.toString());
+        response.body.data[0].attributes.application.should.equal('rw');
+        response.body.data[0].attributes.resourceType.should.equal(favouriteOne.resourceType);
+        response.body.data[0].attributes.resourceId.should.equal(favouriteOne.resourceId);
+
+        response.body.data[1].id.should.equal(favouriteTwo._id.toString());
+        response.body.data[1].attributes.application.should.equal('rw');
+        response.body.data[1].attributes.resourceType.should.equal(favouriteTwo.resourceType);
+        response.body.data[1].attributes.resourceId.should.equal(favouriteTwo.resourceId);
+
+        response.body.data[2].id.should.equal(favouriteThree._id.toString());
+        response.body.data[2].attributes.application.should.equal('gfw');
+        response.body.data[2].attributes.resourceType.should.equal(favouriteThree.resourceType);
+        response.body.data[2].attributes.resourceId.should.equal(favouriteThree.resourceId);
+    });
+
     it('Find favourite by userId without any favourites returns empty list', async () => {
         mockGetUserFromToken(USERS.USER);
 
